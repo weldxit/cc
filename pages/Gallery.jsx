6b04912e5209cdcd974ec4b8d6c0cdfe10eb660b
srilekha <114071074+Omweldxit@users.noreply.github.com>
2022-12-08 19:07:17 +0530

    gallery

--- pages/Gallery.jsx
+++ pages/Gallery.jsx
@@ -5,47 +5,49 @@ import style from "../styles/gallery.module.css";
 export default function Gallery() {
   return (
     <Layout>
+      <div className={style.main}>
       <div className={style.container}>
         <div className={style.gallery_text}>
           <h1 className={style.gallery_h1}>See Our Images</h1>
         </div>
       <div className={style.gallery}>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects3.jpg"} width={300} height={420} alt="bio" className={style.image} />
+        <Image src={"/bf.jpg"} width={240} height={240} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects1.jpg"} width={580} height={420} alt="bio" className={style.image} />
+        <Image src={"/bf.jpg"} width={240} height={240} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects6.jpg"} width={300} height={420} alt="bio" className={style.image} />
+        <Image src={"/bf.jpg"} width={240} height={240} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects10.jpg"} width={340} height={420} alt="bio" className={style.image} />
+        <Image src={"/bf.jpg"} width={240} height={240} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects7.jpg"} width={500} height={420} alt="bio" className={style.image} />
+        <Image src={"/bf.jpg"} width={240} height={240} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects8.jpg"} width={400} height={430} alt="bio" className={style.image} />
+        <Image src={"/bf.jpg"} width={240} height={240} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects12.jpg"} width={300} height={430} alt="bio" className={style.image} />
+        <Image src={"/bf.jpg"} width={240} height={240} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects13.jpg"} width={300} height={430} alt="bio" className={style.image} />
+        <Image src={"/bf.jpg"} width={240} height={240} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects17.jpg"} width={340} height={430} alt="bio" className={style.image} />
+        <Image src={"/bf.jpg"} width={240} height={240} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects19.jpg"} width={420} height={430} alt="bio" className={style.image} />
+        <Image src={"/bf.jpg"} width={240} height={240} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects33.jpg"} width={300} height={430} alt="bio" className={style.image} />
+        <Image src={"/bf.jpg"} width={240} height={240} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects34.jpg"} width={420} height={430} alt="bio" className={style.image} />
+        <Image src={"/bf.jpg"} width={240} height={240} alt="bio" className={style.image} />
         </div>
+      </div>
       </div>
       </div>
     </Layout>
