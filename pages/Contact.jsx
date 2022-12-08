dd29b2d41762ac6a8904c26135128a0fdce527cc
Subhransu Bidyasagar Kandi <subhransubidyasagar7@gmail.com>
2022-12-08 19:10:26 +0530

    added

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import Form from './Form'
-import Layout from './Layout'
-
+import React from "react";
+import Form from "./Form";
+import Layout from "./Layout";
+import style from '../styles/contact.module.css'
 
 export default function Contact() {
   return (
     <Layout>
-<Form/>
-    
+      <div className={style.contact}>
+        <div className={style.texts}>
+          <h1 className={style.texts_h1}>Contact Us</h1>
+          <p className={style.texts_p}>
+            Contact to Chasicare Expert team by filling out the form below and
+            our expert team will reach you soon. As a farmer’s centric
+            organisation, the CHASICARE in Odisha is committed to help farmers
+            for setting up their organic farm, biofloc fish farm, dairy farm,
+            goatery farm, aquaponics and more.
+          </p>
+        </div>
+        <div className={style.address}>
+          <div className={style.address_add}></div>
+          <div className={style.address_mob}></div>
+          <div className={style.address_mail}></div>
+        </div>
+
+        <Form />
+      </div>
     </Layout>
-  )
+  );
 }
--- pages/Gallery.jsx
+++ pages/Gallery.jsx
@@ -11,40 +11,40 @@ export default function Gallery() {
         </div>
       <div className={style.gallery}>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects3.jpg"} width={300} height={420} alt="bio" className={style.image} />
+        <Image src={'/bf.jpg'} width={300} height={420} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects1.jpg"} width={580} height={420} alt="bio" className={style.image} />
+        <Image src={'/bf.jpg'} width={580} height={420} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects6.jpg"} width={300} height={420} alt="bio" className={style.image} />
+        <Image src={'/bf.jpg'} width={300} height={420} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects10.jpg"} width={340} height={420} alt="bio" className={style.image} />
+        <Image src={'/bf.jpg'} width={340} height={420} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects7.jpg"} width={500} height={420} alt="bio" className={style.image} />
+        <Image src={'/bf.jpg'} width={500} height={420} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects8.jpg"} width={400} height={430} alt="bio" className={style.image} />
+        <Image src={'/bf.jpg'} width={400} height={430} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects12.jpg"} width={300} height={430} alt="bio" className={style.image} />
+        <Image src={'/bf.jpg'} width={300} height={430} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects13.jpg"} width={300} height={430} alt="bio" className={style.image} />
+        <Image src={'/bf.jpg'} width={300} height={430} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects17.jpg"} width={340} height={430} alt="bio" className={style.image} />
+        <Image src={'/bf.jpg'} width={340} height={430} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects19.jpg"} width={420} height={430} alt="bio" className={style.image} />
+        <Image src={'/bf.jpg'} width={420} height={430} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects33.jpg"} width={300} height={430} alt="bio" className={style.image} />
+        <Image src={'/bf.jpg'} width={300} height={430} alt="bio" className={style.image} />
         </div>
         <div className={style.g_image}>
-        <Image src={"/watermarkpics/Chasicare_Biofloc_Projects34.jpg"} width={420} height={430} alt="bio" className={style.image} />
+        <Image src={'/bf.jpg'} width={420} height={430} alt="bio" className={style.image} />
         </div>
       </div>
       </div>
